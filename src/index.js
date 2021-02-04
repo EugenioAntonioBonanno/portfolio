bf08092688df74cc1dc55bffc374b562504ba261
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-04 17:13:57 +0800

    add photo for header to project

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Header from './header';
 
 function App() {
   return (
-    <div className="App bg-blue-500">
+    <div>
+      <Header />
       <div className='mt-10'> I AM ALIVE</div>
     </div>
   );
@@ -18,7 +20,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
