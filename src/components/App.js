05bce2ad48840c6f6e668c6008d5851273550352
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-19 13:25:38 +0800

    add scaffolding for page components

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Header from './Header';
-import About from './pages/About'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div>
-      <Header />
-      <main className="w-full flex justify-center">
-        <div className='w-full max-w-screen-2xl'>
-          <About />
+      <BrowserRouter>
+        <div>
+        <Header />
+        <main className="w-full flex justify-center">
+          <div className='w-full max-w-screen-2xl'>
+            <About />
+          </div>
+        </main>
+
         </div>
-      </main>
+      </BrowserRouter>
     </div>
   );
 }
