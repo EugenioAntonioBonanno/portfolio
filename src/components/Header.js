e4ca187fe1e464cb1208852f093fae7ab46a14af
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-19 21:11:56 +0800

    remove uses of class and replace with className

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ import resume from '../assets/gino_resume.pdf';
 
 const Header = () => {
   return (
-    <header class="bg-blue-100 py-3 w-screen shadow-md flex justify-center items-center">
-      <div class="flex w-full max-w-screen-xl items-center justify-between px-4 sm:px-4">
+    <header className="bg-blue-100 py-3 w-screen shadow-md flex justify-center items-center">
+      <div className="flex w-full max-w-screen-xl items-center justify-between px-4 sm:px-4">
 
         <div>
           <Link to="/">
@@ -17,8 +17,8 @@ const Header = () => {
           </Link>
         </div>
 
-        <div class="sm:hidden">
-          <button type="button" id='toggle-menu-options' class="block text-gray-600 hover:text-grey-800 focus:text-grey-900 focus:outline-none">
+        <div className="sm:hidden">
+          <button type="button" id='toggle-menu-options' className="block text-gray-600 hover:text-grey-800 focus:text-grey-900 focus:outline-none">
             <svg className="h-6 w-6 mr-3 fill-current" viewBox="0 0 24 24">
               <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
             </svg>
