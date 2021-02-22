24836e333a1729e2d845f2e9fb5634f9c626c49d
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-22 13:57:47 +0800

    add  photos and  adjust card styles

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const ImageCard = (props) => {
   return (
-    <div className="max-w-sm my-7 mx-5 bg-gray-100 rounded overflow-hidden shadow-lg">
-      <img src={props.img} alt='of project'></img>
-      <div className="px-6 py-4">
+    <div className="max-w-md w-full py-5 px-7 my-7 mx-5 bg-gray-100 rounded overflow-hidden shadow-lg">
+      <section className='flex justify-center'>
+        <img src={props.img} alt='of project' className='max-h-56 px-3 py-1'></img>
+      </section>
+      <div className="px-4 py-5">
         <div className="font-bold text-indigo-500 text-xl mb-2">
           {props.title}
         </div>
-        <p className="text-gray-700 text-base">
+        <p className="text-gray-700 ml-2 text-base">
           {props.writeUp}
         </p>
       </div>
--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -2,43 +2,46 @@ import React from 'react'
 import ImageCard from '../ImageCard';
 
 import hireFullstackImage from '../../assets/hire-fullstack-image.png';
+import traceOutline from '../../assets/traceoutline.png';
+import orderMagik from '../../assets/orderMagik.png';
+import launchSchool from '../../assets/outlineLaunch.png';
 
 
 const Experience = () => {
   return (
     <div className='flex flex-wrap w-full mt-16 justify-center mx-3'>
       <ImageCard
-        img = {hireFullstackImage}
+        img = {traceOutline}
+        title = 'Trace Taiwan'
+        writeUp='This is the write up'
+        tags={['gino', 'dog', 'cat']}
+      />
+
+      <ImageCard
+        img = {orderMagik}
         title = 'THIS IS THE TITLE'
-        writeup='This is the write up'
+        writeUp='This is the write up'
+        tags={['gino', 'dog', 'cat']}
+      />
+
+      <ImageCard
+        img = {launchSchool}
+        title = 'THIS IS THE TITLE'
+        writeUp='This is the write up'
         tags={['gino', 'dog', 'cat']}
       />
 
       <ImageCard
         img = {hireFullstackImage}
         title = 'THIS IS THE TITLE'
-        writeup='This is the write up'
+        writeUp='This is the write up'
         tags={['gino', 'dog', 'cat']}
       />
 
       <ImageCard
         img = {hireFullstackImage}
         title = 'THIS IS THE TITLE'
-        writeup='This is the write up'
-        tags={['gino', 'dog', 'cat']}
-      />
-
-      <ImageCard
-        img = {hireFullstackImage}
-        title = 'THIS IS THE TITLE'
-        writeup='This is the write up'
-        tags={['gino', 'dog', 'cat']}
-      />
-
-      <ImageCard
-        img = {hireFullstackImage}
-        title = 'THIS IS THE TITLE'
-        writeup='This is the write up'
+        writeUp='This is the write up'
         tags={['gino', 'dog', 'cat']}
       />
     </div>
