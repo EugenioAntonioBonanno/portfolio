e8f99d62ea3efc6d0df0c0f3340ddaaa6502105b
Eugenio Antonio Bonanno <eugenioantoniobonanno@gmail.com>
2021-02-22 23:32:24 +0800

    adjust percentage bars

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const ProgressBar = (props) => {
   return (
-    <div className="flex items-center my-2 w-full pr-2 rounded bg-gray-400">
-      <span className='inline-block rounded rounded-r-none font-medium px-2 w-32 text-gray-700 bg-indigo-400'>{props.skill}</span>
-      <div className='inline-block relative w-full'>
-        <span className="text-center absolute right-0 font-medium text-gray-700">{props.percentage}%</span>
+    <div className="flex items-center my-3 w-full h-9 pr-2 rounded-md bg-gray-400">
+      
+      <span className='inline-flex items-center rounded-md rounded-r-none font-extrabold pl-2 h-full w-44 text-indigo-600 bg-gray-300'>{props.skill}</span>
+      <div className='inline-flex items-center relative h-full w-full'>
+        <span className="inline-flex absolute right-0 font-semibold text-indigo-600">{props.percentage}%</span>
 
-        <section className="bg-green-300  inline-block" style={{width: `${props.percentage}%`}}>
+        <section className="bg-green-400 h-full inline-block" style={{width: `${props.percentage}%`}}>
           &nbsp;
         </section>
       </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import ProgressBar from '../ProgressBar';
+
 import me from '../../assets/me.png';
 import SvgInfo from '../SvgInfo';
 import Breifcase from '../svgs/Briefcase';
@@ -15,16 +18,20 @@ const About = () => {
 
       <div className='block md:flex justify-around mx-3 mt-12'>
 
-        <div className='grid-cols-1 max-w-lg auto-cols-min'>
+        <div className='grid-cols-1 max-w-lg mb-7 auto-cols-min'>
           
           <div className='w-full flex justify-center'>
             <img src={me} alt="full-stack" className='max-h-96'></img>
           </div>
-          <div className='w-full flex mb-3 justify-center'>
+          <div className='w-full flex mb-5 justify-center'>
             <h1 className='text-5xl font-semibold text-gray-700'>Who am I?</h1>
           </div>
-          <div className='w-full flex justify-center'>
-            <p className='text-center'>I am a full-stack web developer with a genuine love for web development (and my laptop).</p>
+          <div className='w-full flex mx-2 justify-center'>
+            <p className='text-center text-lg text-gray-800'>
+              I am a full-stack developer with a genuine love for coding (and my laptop). I enjoy solving interesting problems,
+              and building user friendly web applications. If that sounds like something you need help with please dont hesistate to 
+              <span> </span><Link to='/contact' className="inline underline text-indigo-700">contact me</Link>.
+            </p>
           </div>
           
         </div>
@@ -32,16 +39,21 @@ const About = () => {
 
         <div className='w-full max-w-lg'>
           <div className=''>
-            <h2 className='text-2xl font-bold text-gray-800'>Gino Bonanno</h2>
+            <h2 className='text-4xl my-5 font-bold text-gray-700'>Gino Bonanno</h2>
             <div className='ml-3 shadow-sm'>
               <ProgressBar 
-                percentage='85' 
-                skill='Ruby'
+                percentage='80' 
+                skill='Ruby/Rails'
               />
 
               <ProgressBar 
                 percentage='75' 
                 skill='JavaScript'
+              />
+
+              <ProgressBar 
+                percentage='60' 
+                skill='React'
               />
 
               <ProgressBar 
@@ -63,13 +75,23 @@ const About = () => {
                 percentage='99' 
                 skill='Desire'
               />
+
+              <ProgressBar 
+                percentage='90' 
+                skill='Communication'
+              />
+
+              <ProgressBar 
+                percentage='0' 
+                skill='Quit'
+              />
             </div>
           </div>
         </div>
       </div>
 
       <div className='flex justify-center mb-5'>
-        <div className='w-full grid grid-cols-2 lg:grid-cols-4 justify-items-center md:mt-24'>
+        <div className='w-full grid grid-cols-2 lg:grid-cols-4 justify-items-center md:mt-12'>
           <SvgInfo
             svg={<Shield
                   color={svgColor}
